843d78300be55419c39cfb240af952daffeedbac
Zig2007 <enzo.i.paula@aluno.senai.br>
2025-11-17 14:25:45 -0300

    Add files via upload

---
+++ m5-main/7.js
@@ -0,0 +1,5 @@
+const greet = function(name) { //atribui à constante greet uma function expression recebendo name.
+  return "Olá, " + name + "!"; //constrói e retorna a saudação.
+};
+
+console.log(greet("Enzo")); //chama greet e imprime o resultado
